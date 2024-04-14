fa11a98d57326b249f7f6b2bf9fa63bc63282899
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-04-14 11:34:50 -0300

    feat: second test with token credit card

---
+++ src/RegisterCard.Test/TokenCreditCardTest.cs
@@ -0,0 +1,22 @@
+﻿using RegisterCard.Application.Services;
+
+namespace RegisterCard.Test;
+public class TokenCreditCardTest
+{
+    [Theory]
+    [InlineData("1234567891234567", 0001, "7456")]
+    [InlineData("1234567891234567", 0002, "6745")]
+    [InlineData("1234567891234567", 0003, "5674")]
+    [InlineData("1234567891234567", 0004, "4567")]
+    [InlineData("1234567891234567", 9999, "5674")]
+    public void ShouldCreateAValidToken(string creditCardNumber, short securityCode, string expectedToken)
+    {
+        var generator = new CreditCardTokenGenerator();
+        var token = generator.Generate(
+            new BeloToken(),
+            new Core.Entities.CreditCard(creditCardNumber, securityCode)
+            );
+
+        Assert.Equal(expectedToken, token.Number);
+    }
+}
